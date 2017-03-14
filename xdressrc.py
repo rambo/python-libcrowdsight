06104a38d0041beffd08003af5c1705d72dc2e2c
Eero af Heurlin <eero.afheurlin@iki.fi>
2017-03-14 17:56:37 +0200

    keep the OpenCV types out for now, they cause choke

--- xdressrc.py
+++ xdressrc.py
@@ -3,11 +3,18 @@ package = 'crowdsight'     # top-level python package name
 packagedir = 'crowdsight'  # location of the python package
 
 stlcontainers = [
-    ('vector', 'str'),
-    ('set', 'uint'),
-    ('map', 'int', 'float'),
-    ]
+    ('vector', 'int'),
+    ('vector', 'float'),
+    ('vector', ('vector', 'int')),
+#    'cv::Point',
+#    'cv::Mat',
+#    'cv::Rect',
+#    ('vector', 'cv::Point'),
+#    ('vector', 'cv::Rect'),
+]
 
 # will be used later, but need to be present now
-classes = []
+classes = [
+    ('*', '/usr/include/crowdsight.h'),
+]
 functions = []
